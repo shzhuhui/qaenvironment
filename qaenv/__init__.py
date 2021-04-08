46c94d44e78b45b01cccdc902a4c8235dfd307d3
zhuhui <zhuhui@192.168.0.103>
2021-04-08 21:10:59 +0800

    MAC

--- qaenv/__init__.py
+++ qaenv/__init__.py
@@ -5,12 +5,12 @@ __author__ = 'yutiansut'
 __version__ = '0.0.2'
 
 
-mongo_ip = os.getenv('MONGODB', '127.0.0.1')
+mongo_ip = os.getenv('MONGODB', '192.168.0.151')
 mongo_port = 27017
 mongo_uri = 'mongodb://{}:{}'.format(mongo_ip, mongo_port)
 
 
-eventmq_ip = os.getenv('QAPUBSUB_IP', '127.0.0.1')
+eventmq_ip = os.getenv('QAPUBSUB_IP', '192.168.0.151')
 eventmq_port = os.getenv('QAPUBSUB_PORT', 5672)
 eventmq_username = os.getenv('QAPUBSUB_USER', 'admin')
 eventmq_password = os.getenv('QAPUBSUB_PWD', 'admin')
@@ -19,6 +19,6 @@ eventmq_amqp = 'pyamqp://{}:{}@{}:{}//'.format(
 
 
 
-clickhouse_ip =  os.getenv('CLICKHOUSE_IP', '127.0.0.1')
+clickhouse_ip =  os.getenv('CLICKHOUSE_IP', '192.168.0.151')
 
-redis_ip = os.getenv('REDIS_IP', '127.0.0.1')
+redis_ip = os.getenv('REDIS_IP', '192.168.0.151')
